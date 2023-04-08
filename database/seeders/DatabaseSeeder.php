b2dd193672d36ee23f59e52fd38cb6b38bd83d5d
GeorgeAntwanHosny <georgeantwanhosnyghaly@outlook.com>
2023-04-08 15:03:49 +0200

    add my blog app

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::factory()->create([
+            'name' => 'John Doe'
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+
+        //TODO try to provide multiple user_id in Post Factory and also in Comment
+    }
+}
